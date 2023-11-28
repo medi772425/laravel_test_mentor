d9a2857ce6f487c3b2b6573220516d377149c94c
medi772425 <medi772425@gmail.com>
2023-11-28 21:28:34 +0900

    すべてのカラムを取得するように変更

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -4,7 +4,7 @@ namespace App\Repositories;
 use App\Models\User;
 
 class UserRepository {
-    public function get() {
-        return User::select('id', 'name', 'created_at', 'updated_at')->get();
+    public function findAll() {
+        return User::select('*')->get();
     }
 }
